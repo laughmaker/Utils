1a7578cd2cdd22afa9a8e882d7cad49d562b9e98
wskideal <wskrdfeai@gmail.com>
2013-05-27 15:15:04 +0800

    Initial Commit

---
+++ UtilsDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UtilsDemo
+//
+//  Created by line0 on 13-5-27.
+//  Copyright (c) 2013年 makeLaugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UtilsDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UtilsDemo
+//
+//  Created by line0 on 13-5-27.
+//  Copyright (c) 2013年 makeLaugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
